89e24b372acc89f2985d197a81387124a427c613
Hoooao <hao021014@163.com>
2023-07-30 18:39:27 -0400

    Init test with apt for infrap4d deps

---
+++ tools/install_infrap4d_deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# This script should only be used in Ubuntu22.04
+
+DEPS="libabsl-dev \
+      libc-ares-dev=1.18.1-1ubuntu0.22.04.2 \
+      libcctz-dev \
+      libgflags-dev \
+      libgoogle-glog-dev \
+      libgrpc++-dev \
+      libgtest-dev=1.11.0-3 \
+      nlohmann-json3-dev \
+      libprotobuf-dev \
+      zlib1g-dev"
+
+sudo apt-get update
+sudo apt-get install -y ${DEPS}
+
+
+
+
+
+
+
+
+
