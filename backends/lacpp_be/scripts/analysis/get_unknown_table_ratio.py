dde03e88233f130bc88c2e3e988e41da7fb02b91
Hoooao <hao021014@163.com>
2025-06-04 21:11:19 -0400

    analysis tools

---
+++ backends/lacpp_be/scripts/analysis/get_unknown_table_ratio.py
@@ -0,0 +1,29 @@
+# this script is used to get the ratio of unknown tables to total tables in a program
+# try to see if there is a correlation between the accuracy of prediction (mape)
+# and the ratio of unknown tables.
+
+#input: dir containing json files for features
+import os
+import json
+import argparse
+parser = argparse.ArgumentParser(description="Get the ratio of unknown tables in JSON files.")
+parser.add_argument("--dir", type=str, default="data/", help="Directory containing JSON files.")
+
+
+for root, dirs, files in os.walk(parser.parse_args().dir):
+    for file in files:
+        if file.endswith(".json"):
+            tables = []
+            with open(os.path.join(root, file), "r") as f:
+                data = json.load(f)
+                if "nodes" not in data:
+                    continue
+                num_total_nodes = len(data["nodes"])
+                unknown_cnt = 0
+                for node_attr in data["node_attr"]:
+                    if node_attr[5] == 1:
+                        unknown_cnt += 1
+            ratio = (num_total_nodes - unknown_cnt) / num_total_nodes
+            print(f"{file}: ratio: {ratio:.2f} unknown_cnt: {unknown_cnt} total: {num_total_nodes}")
+
+                    
